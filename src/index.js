afd7c95b7209807741aa1c5f38962850be7e602c
Sergio Xalambrí <hello@sergiodxa.com>
2018-11-22 15:53:15 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const hasOnMethod = object => typeof object.on === "function";
+const hasAddEventListenerMethod = object =>
+  typeof object.addEventListener === "function";
+
+const hasOffMethod = object => typeof object.off === "function";
+const hasRemoveEventListenerMethod = object =>
+  typeof object.removeEventListener === "function";
+
+export function removeListener(object, event, callback) {
+  if (hasOffMethod(object)) {
+    return object.off(event, callback);
+  }
+  if (hasRemoveEventListenerMethod(object)) {
+    return object.removeEventListener(event, callback);
+  }
+}
+
+export function addListener(object, event, callback) {
+  if (hasOnMethod(object)) {
+    return object.on(event, callback);
+  }
+  if (hasAddEventListenerMethod(object)) {
+    return object.addEventListener(event, callback);
+  }
+}
+
+export function subscribe(object, event, callback) {
+  const unsubscribe = addListener(object, event, callback);
+  return () => {
+    if (subscribe && typeof unsubscribe === "function") {
+      return unsubscribe();
+    }
+    return removeListener(object, event, callback);
+  };
+}
+
+export default subscribe;
